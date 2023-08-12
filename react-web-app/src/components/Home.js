5a70d4714474db6d605ad43613b5d922771ad681
keiichi031605 <keichi031605@gmail.com>
2023-08-12 21:50:51 +1000

    Update Home.js

--- react-web-app/src/components/Home.js
+++ react-web-app/src/components/Home.js
@@ -10,10 +10,10 @@ export default function Home() {
     <React.Fragment>
       <Box className="container">
         <Typography className="home-text" variant="h3" component="h3" sx={{ color: 'white' }}>
-          Can Make Coffee Improvement Easy
+          Make Coffee Improve Easy
         </Typography>
         <Typography className="home-text" variant="h5" component="h5" sx={{ color: 'gray' }}>
-          Get the easy-cupping tool, manageable your roasting profile and cupping result to efficiently improve your daily workflow.
+          Get the easy-cupping tool with manageable your roasting profile and cupping result to efficiently improve your daily workflow.
         </Typography>
         <Button className="get-started-button" size="large" variant="contained" color="primary">
           Get Started
