ecf9c0972cc04632b5ea9642825747996df984bc
keiichi031605 <keichi031605@gmail.com>
2023-08-12 11:34:13 +1000

    annotated coffee model

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: coffees
+#
+#  id          :bigint           not null, primary key
+#  name        :string           not null
+#  price       :integer
+#  process     :string
+#  variety     :string
+#  roasted_for :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Coffee < ApplicationRecord
 end
