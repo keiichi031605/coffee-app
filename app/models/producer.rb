1afa23ef204913aae746bc3aa7a43e3c9da860a4
keiichi031605 <keichi031605@gmail.com>
2023-08-12 14:17:15 +1000

    setup devise authenticatio
    https://sdrmike.medium.com/rails-7-api-only-app-with-devise-and-jwt-for-authentication-1397211fb97c

--- app/models/producer.rb
+++ app/models/producer.rb
@@ -8,6 +8,7 @@
 #  location   :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :bigint
 #
 class Producer < ApplicationRecord
   has_many :coffees
---
+++ app/models/user.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                     :bigint           not null, primary key
+#  name                   :string
+#  email                  :string           default(""), not null
+#  encrypted_password     :string           default(""), not null
+#  reset_password_token   :string
+#  reset_password_sent_at :datetime
+#  remember_created_at    :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  jti                    :string           not null
+#
+class User < ApplicationRecord
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  devise :database_authenticatable, :registerable, :recoverable, :validatable, :jwt_authenticatable, jwt_revocation_strategy: self
+end
