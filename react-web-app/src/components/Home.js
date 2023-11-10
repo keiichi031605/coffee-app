316217c5c0187c3f014a23a1f7b909fa914a8d16
keiichi031605 <keichi031605@gmail.com>
2023-11-10 12:31:07 +1100

    moved onboarding page to a child component

--- react-web-app/src/components/Home.js
+++ react-web-app/src/components/Home.js
@@ -1,5 +1,6 @@
 import * as React from "react";
 import '../styles/home.css'
+import Onboarding from './Onboarding.js'
 // import { Link } from "react-router-dom";
 import { Container, Box, Typography, Button } from '@mui/material';
 
@@ -8,22 +9,7 @@ export default function Home() {
   return (
     <React.Fragment>
       <Container component="main">
-        <Box className="home-wrapper">
-          <Typography className="home-text" variant="h3" component="h3" sx={{ color: 'white' }}>
-            Make Coffee Improve Easy
-          </Typography>
-          <Typography className="home-text" variant="h5" component="h5" sx={{ color: 'gray' }}>
-            Get the easy-cupping tool with manageable your roasting profile and cupping result to efficiently improve your daily workflow.
-          </Typography>
-          <Button className="get-started-button" size="large" variant="contained" color="primary">
-            Get Started
-          </Button>
-          <img
-            className="home-image"
-            src="/images/home-image.png"
-            alt="home"
-          />
-        </Box>
+        <Onboarding />
       </Container>
     </React.Fragment>
   );
