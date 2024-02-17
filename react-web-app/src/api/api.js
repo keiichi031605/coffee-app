e42c3c08c333ec28e0ad5cf8be7649a4a7bba4b0
Keiichi Katsuno <keichi031605@gmail.com>
2024-02-17 20:50:18 +1100

    handle login with jwt token

--- react-web-app/src/api/api.js
+++ react-web-app/src/api/api.js
@@ -7,4 +7,15 @@ const instance = axios.create({
   }
 });
 
+// authentication with jwt token
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('jwt');
+  if (token) {
+    config.headers['Authorization'] = token;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
 export default instance;
