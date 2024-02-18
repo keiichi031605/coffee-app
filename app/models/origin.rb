0b45a7acdabd309b8d995770a6a51a4d35150778
Keiichi Katsuno <keichi031605@gmail.com>
2024-02-18 17:30:21 +1100

    add a enum countries for origin model

--- app/models/origin.rb
+++ app/models/origin.rb
@@ -10,4 +10,27 @@
 #
 class Origin < ApplicationRecord
   has_many :coffees
+
+  enum country: {
+    brazil: 0,
+    vietnam: 1,
+    colombia: 2,
+    indonesia: 3,
+    ethiopia: 4,
+    honduras: 5,
+    india: 6,
+    uganda: 7,
+    mexico: 8,
+    peru: 9,
+    guatemala: 10,
+    nicaragua: 11,
+    costa_rica: 12,
+    kenya: 13,
+    yemen: 14,
+    tanzania: 15,
+    panama: 16,
+    el_salvador: 17,
+    rwanda: 18,
+    burundi: 19
+  }
 end
